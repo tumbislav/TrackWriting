e15ba8cb95adc0ddbf4bb52bd3d3c8c98981d7c0
mcibej <r6zXpznE3uGMorTa29jv>
2019-01-23 20:08:12 +0100

    update

--- source/dbs.py
+++ source/dbs.py
@@ -8,72 +8,63 @@ Wraps a single database and exposes only those operations that we need.
 import json
 import os
 import sqlite3
-from typing import List
+from typing import ClassVar
 
 
-_CREATE_TABLES = '''
-    create table works (
-        id integer primary key, 
-        work text not null
-    );
-    create table history (
-        id integer primary key, 
-        work text not null
-    );
-'''
+class Database:
+    initialize_sql: ClassVar[str] = '''
+        create table works (
+            id integer primary key, 
+            work text not null
+        );
+        create table history (
+            id integer primary key, 
+            history_work integer not null,
+            foreign key(history_work) references works(id),
+            value_type text,
+            timestamp text,
+            value text
+        );
+    '''
+    insert_work_sql: ClassVar[str] = 'insert into works (work) values (?)'
 
+    con: sqlite3.Connection
 
-con: sqlite3.Connection
+    def open_db(self, file_name: str):
+        """
+        Opens or creates a database. This class only wraps a single database, so if one is already open,
+        this method raises an exception.
+        :param file_name: the file to use
+        """
+        if self.con is not None:
+            raise Exception('Attempting to open a database when one is already open.')
+        db_exists = os.path.isfile(file_name)
+        self.con = sqlite3.connect(file_name)
+        if not db_exists:
+            cursor = self.con.cursor()
+            cursor.execute(self.initialize_sql)
+            self.con.commit()
 
+    def close_db(self):
+        """
+        Flushes and closes the opened database.
+        """
+        if self.con is None:
+            raise Exception('No database is open.')
+        self.con.commit()
+        self.con.close()
+        self.con = None
 
-def open_db(file_name: str):
-    """
-    Opens or creates a database. This class only wraps a single database, so if one is already open,
-    this method raises an exception.
-    :param file_name: the file to use
-    """
-    if con is not None:
-        raise Exception('Attempting to open a database when one is already open.')
-    db_exists = os.path.isfile(file_name)
-    con = sqlite3.connect(file_name)
-    if not db_exists:
-        initialize()
-
-
-def close():
-    """
-    Flushes and closes the opened database.
-    """
-    global _con
-    if _con is None:
-        raise Exception('No database is open.')
-    _con.commit()
-    _con.close()
-    _con = None
-
-
-def initialize():
-    """
-    Creates the database structure.
-    """
-    cursor = _con.cursor()
-    cursor.execute('create table works (id integer PRIMARY KEY, work text NOT NULL)')
-    cursor.execute('create table history (id integer PRIMARY KEY, work text NOT NULL)')
-
-
-def dump_to_sqlite():
-    with open('c:/Users/mcibej/Work-synced/Writing/raw-track.json', 'r') as f:
-        d = json.load(f)
-    con = sqlite3.connect(r'c:\Users\mcibej\Work-synced\Writing\db.sqlite3')
-    cursor = con.cursor()
-    cursor.execute('create table works (id integer PRIMARY KEY, work text NOT NULL)')
-    insert_sql = 'INSERT INTO works (work) VALUES (?)'
-    for w in d['works']:
-        cursor.execute(insert_sql, (json.dumps({'name': w['name'],
-                                                'world': w['world'],
-                                                'series': w['series'],
-                                                'genre': w['genre'],
-                                                'type': w['type'],
-                                                'status': w['status'],
-                                                'word_count': w['word_count']}),))
-    con.commit()
+    def load_from_json(self, json_file: str):
+        with open(json_file, 'r') as f:
+            d = json.load(f)
+        cursor = self.con.cursor()
+        for w in d['works']:
+            cursor.execute(self.insert_work_sql,
+                           (json.dumps({'name': w['name'],
+                                        'world': w['world'],
+                                        'series': w['series'],
+                                        'genre': w['genre'],
+                                        'type': w['type'],
+                                        'status': w['status'],
+                                        'word_count': w['word_count']}),))
