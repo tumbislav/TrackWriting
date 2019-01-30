24f3d5063db6000f2678dfab53301bd5511097d3
mcibej <r6zXpznE3uGMorTa29jv>
2019-01-30 20:09:24 +0100

    update

--- source/static/adapter.js
+++ source/static/adapter.js
@@ -10,115 +10,118 @@
  * The entire application is packaged in the 'writing' namespace.
  */
 var writing = (function writing(self, $) {
-  'use strict';
+    'use strict';
 
-  self.Works = function(view) {
-    // the model's data cache
-    this.works = null;
-    this.classifiers = null;
-    this.translations = null;
-    // the configuration
-    this.preferred_language = 'en';
-    this.current_translation = null;
+    self.Works = function (view) {
+        // the model's data cache
+        this.works = null;
+        this.classifiers = null;
+        this.translations = null;
+        // the configuration
+        this.preferred_language = 'en';
+        this.current_translation = null;
 
-    // Events we generate ourselves.
-    // initial load or reload
-    this.onReload = new self.Event(this);
-  };
+        // Events we generate ourselves.
+        // initial load or reload
+        this.onReload = new self.Event(this);
+    };
 
-  // the main functionality
-  self.Works.prototype = {
+    // the main functionality
+    self.Works.prototype = {
 
-    import() { $.get('/import', (data, status) => this.reload() ); },
+        import() {
+            $.get('/import', (data, status) => this.reload());
+        },
 
-    attachView(view) {
-      // adding a new work
-      if (view.hasOwnProperty('onAdd')) {
-        view.onAdd.attach( (sender, data) => this.addWork(data) ); }
-      // deleting a work
-      if (view.hasOwnProperty('onDelete')) {
-        view.onDelete.attach( (sender, data) => this.deleteWork(data) ); }
-      // changing an existing work
-      if (view.hasOwnProperty('onUpdate')) {
-        view.onUpdate.attach( (sender, data) => this.updateWork(data) ); }
-    },
+        attachView(view) {
+            // adding a new work
+            if (view.hasOwnProperty('onAdd')) {
+                view.onAdd.attach((sender, data) => this.addWork(data));
+            }
+            // deleting a work
+            if (view.hasOwnProperty('onDelete')) {
+                view.onDelete.attach((sender, data) => this.deleteWork(data));
+            }
+            // changing an existing work
+            if (view.hasOwnProperty('onUpdate')) {
+                view.onUpdate.attach((sender, data) => this.updateWork(data));
+            }
+        },
 
-    // clear current data and reload everything from server, then tell the views to refresh
-    reload() {
-      let _this = this;
-      $.when(
-        $.get('/works', function(response){
-          _this.works = JSON.parse(response);
-        }),
-        $.get('/classifiers', function(response){
-          _this.classifiers = JSON.parse(response);
-        }),
-        $.get('/translations', function(response){
-          _this.translations = JSON.parse(response);
-          if (_this.preferred_language != null && _this.translations.hasOwnProperty(_this.preferred_language)) {
-            _this.current_translation = _this.translations[_this.preferred_language];
-          }
-        })
-      ).then(function(){
-        _this.onReload.notify();
-      });
-    },
+        // clear current data and reload everything from server, then tell the views to refresh
+        reload() {
+            let _this = this;
+            $.when(
+                $.get('/works', function (response) {
+                    _this.works = JSON.parse(response);
+                }),
+                $.get('/classifiers', function (response) {
+                    _this.classifiers = JSON.parse(response);
+                }),
+                $.get('/translations', function (response) {
+                    _this.translations = JSON.parse(response);
+                    if (_this.preferred_language != null &&
+                        _this.translations.hasOwnProperty(_this.preferred_language)) {
+                        _this.current_translation = _this.translations[_this.preferred_language];
+                    }})
+            ).then(function () {
+                _this.onReload.notify();
+            });
+        },
 
-    translationContext(context) {
-      return (this.current_translation.hasOwnProperty(context) ? this.current_translation[context] : {});
-    },
+        translationContext(context) {
+            return (this.current_translation.hasOwnProperty(context) ? this.current_translation[context] : {});
+        },
 
-    translateString(context, value) {
-      let ctx = this.translationContext(context);
-      return (ctx.hasOwnProperty(value) ? ctx[value] : value);
-    },
+        translateString(context, value) {
+            let ctx = this.translationContext(context);
+            return (ctx.hasOwnProperty(value) ? ctx[value] : value);
+        },
 
-    translateList(context, values) {
-      let ctx = this.translationContext(context);
-      return values.map((value) => (ctx.hasOwnProperty(value) ? ctx[value] : value))
-    },
+        translateList(context, values) {
+            let ctx = this.translationContext(context);
+            return values.map((value) => (ctx.hasOwnProperty(value) ? ctx[value] : value))
+        },
 
-    translateWork(work) {
-      let adapted = {};
-      adapted.code = work.code;
-      adapted.type = work.type;
-      adapted.name = work.name;
-      adapted.world = work.world;
-      adapted.series = work.series;
-      adapted.genre = this.translateString('genres', work.genre);
-      adapted.form = work.form;
-      adapted.status = work.status;
-      adapted.word_count = work.word_count;
-      return adapted;
-    },
+        translateWork(work) {
+            let adapted = {};
+            adapted.code = work.code;
+            adapted.type = work.type;
+            adapted.name = work.name;
+            adapted.world = work.world;
+            adapted.series = work.series;
+            adapted.genre = this.translateString('genres', work.genre);
+            adapted.form = work.form;
+            adapted.status = work.status;
+            adapted.word_count = work.word_count;
+            return adapted;
+        },
 
-    // get the simple version of the works list
-    getWorks() {
-      let work_list = [];
-      for (let [n, work] of this.works.entries()) {
-        work_list.push(this.translateWork(work));
-      }
-      return work_list;
-    },
+        // get the simple version of the works list
+        getWorks() {
+            let work_list = [];
+            for (let[n, work]of this.works.entries()) {
+                work_list.push(this.translateWork(work));
+            }
+            return work_list;
+        },
 
-    // add a work and notify listeners
-    addWork(work) {
-      $.noop();
-    },
-    deleteWork(work) {
-      $.noop();
-    },
-    updateWork(work) {
-        $.ajax({
+        // add a work and notify listeners
+        addWork(work) {
+            $.noop();
+        },
+        deleteWork(work) {
+            $.noop();
+        },
+        updateWork(work) {
+            $.ajax({
                 url: '/works',
                 type: 'PUT',
                 contentType: 'application/json',
                 data: JSON.stringify(work)
             });
-    },
-  };
+        },
+    };
 
-  return self;
+    return self;
 })(writing || {}, jQuery);
-
-
--- source/static/view.js
+++ source/static/view.js
@@ -2,8 +2,7 @@
  * view.js
  * @author: Marko Čibej
  * @description: all the views, for the time being
-*/
-
+ */
 
 /**
  * The cards view
@@ -12,96 +11,105 @@ var writing = (function writing(self, $) {
     'use strict';
 
     // card deck
-    self.CardsView = function(deck) {
-    let _this = this;
+    self.CardsView = function (deck) {
+        let _this = this;
 
-    // locate and save the templates for DOM elements we use
-    this.deck = deck;
-    this.card_template = $(this.deck.find('#card-template').html());
-    this.sep2_template = $(this.deck.find('#card-sep-2').html());
-    this.sep3_template = $(this.deck.find('#card-sep-3').html());
-    this.sep4_template = $(this.deck.find('#card-sep-4').html());
-    this.sep5_template = $(this.deck.find('#card-sep-5').html());
+        // locate and save the templates for DOM elements we use
+        this.deck = deck;
+        this.card_template = $(this.deck.find('#card-template').html());
+        this.sep2_template = $(this.deck.find('#card-sep-2').html());
+        this.sep3_template = $(this.deck.find('#card-sep-3').html());
+        this.sep4_template = $(this.deck.find('#card-sep-4').html());
+        this.sep5_template = $(this.deck.find('#card-sep-5').html());
 
-    // user events that we handle
-    $('#reload-file').click(function() {
-        _this.adapter.import();
-    });
+        // user events that we handle
+        $('#reload-file').click(function () {
+            _this.adapter.import();
+        });
 
-    deck.on('keydown', '.tw-count', function(e) {
-        let keycode = e.charCode || e.keyCode;
-        if (keycode  == 13) {
-            let card = $(e.target).closest('.card');
-            let new_data = card.data('json');
-            new_data.word_count = Number($(e.target).html());
-            _this.onUpdate.notify(new_data);
-            return false;
-        }
-    });
+        deck.on('keydown', '.tw-count', function (e) {
+            let keycode = e.charCode || e.keyCode;
+            if (keycode == 13) {
+                let card = $(e.target).closest('.card');
+                let new_data = card.data('json');
+                new_data.word_count = Number($(e.target).html());
+                _this.onUpdate.notify(new_data);
+                return false;
+            }
+        });
 
+        // publicise the events we trigger
+        this.onAdd = new self.Event(this);
+        this.onUpdate = new self.Event(this);
+        this.onDelete = new self.Event(this);
 
-    // publicise the events we trigger
-    this.onAdd = new self.Event(this);
-    this.onUpdate = new self.Event(this);
-    this.onDelete = new self.Event(this);
+        // initialize the adapter and attach to it
+        this.adapter = new self.Works();
+        this.adapter.attachView(this);
+        // wire up the events we are interested in
+        if (this.adapter.hasOwnProperty('onReload')) {
+            this.adapter.onReload.attach((sender, data) => this.redisplay())
+        };
 
-    // initialize the adapter and attach to it
-    this.adapter = new self.Works();
-    this.adapter.attachView(this);
-    // wire up the events we are interested in
-    if (this.adapter.hasOwnProperty('onReload')) {
-      this.adapter.onReload.attach( (sender, data) => this.redisplay() ) };
+        // finally, start the initial reload
+        this.adapter.reload();
+    };
 
-    // finally, start the initial reload
-    this.adapter.reload();
-  };
+    self.CardsView.prototype = {
+        translateUI() {
+        },
+        redisplay() {
+            let works = this.adapter.getWorks();
+            let prompt_ids = ['tw-tag-world',
+                'tw-tag-series',
+                'tw-tag-genre',
+                'tw-tag-form',
+                'tw-tag-status',
+                'tw-tag-count',
+                'tw-mnu-reimport'];
 
-  self.CardsView.prototype = {
-    redisplay() {
-      let works = this.adapter.getWorks();
-      let prompt_ids = ['world-prompt',
-                        'series-prompt',
-                        'genre-prompt',
-                        'form-prompt',
-                        'status-prompt',
-                        'count-prompt'];
+            let prompt_values = this.adapter.translateList('ui', prompt_ids);
 
-      let prompt_values = this.adapter.translateList('ui', prompt_ids);
+            for (let i = 0; i < prompt_ids.length; ++i) {
+                this.card_template.find('.' + prompt_ids[i]).html(prompt_values[i]);
+            }
 
-      for (let i = 0; i < prompt_ids.length; ++i) {
-        this.card_template.find('#' + prompt_ids[i]).html(prompt_values[i]);
-      }
+            this.deck.empty();
 
-      this.deck.empty();
+            for (let[row, work]of works.entries()) {
+                let newCard = this.card_template.clone();
+                let nameDiv = newCard.find('.tw-name');
+                nameDiv.html(work.name);
+                if (work.name.length > 50) {
+                    nameDiv.addClass('reduced');
+                } else if (work.name.length > 35) {
+                    nameDiv.addClass('small');
+                }
 
-      for (let [row, work] of works.entries()) {
-        let newCard = this.card_template.clone();
-        newCard.find('#work-title').html(work.name);
-        if (work.name.length > 50) {
-          newCard.find('#work-title').addClass('reduced'); //.css("font-size","70%");
-        }
-        else if (work.name.length > 35) {
-          newCard.find('#work-title').addClass('small'); //.css("font-size","80%");
-        }
+                newCard.find('.tw-world').html(work.world);
+                newCard.find('.tw-series').html(work.series);
+                newCard.find('.tw-genre').html(work.genre);
+                newCard.find('.tw-form').html(work.form);
+                newCard.find('.tw-status').html(work.status);
+                newCard.find('.tw-count').html(work.word_count);
+                newCard.data('json', work);
+                this.deck.append(newCard);
 
-        newCard.find('#count-top').html(work.word_count);
-        newCard.find('#world').html(work.world);
-        newCard.find('#series').html(work.series);
-        newCard.find('#genre').html(work.genre);
-        newCard.find('#form').html(work.form);
-        newCard.find('#status').html(work.status);
-        newCard.find('.tw-count').html(work.word_count);
-        newCard.data('json', work);
-        this.deck.append(newCard);
+                if (row % 2 == 1) {
+                    this.deck.append(this.sep2_template.clone());
+                }
+                if (row % 3 == 2) {
+                    this.deck.append(this.sep3_template.clone());
+                }
+                if (row % 4 == 3) {
+                    this.deck.append(this.sep4_template.clone());
+                }
+                if (row % 5 == 4) {
+                    this.deck.append(this.sep5_template.clone());
+                }
+            }
+        },
+    };
 
-
-        if (row % 2 == 1) { this.deck.append(this.sep2_template.clone()); }
-        if (row % 3 == 2) { this.deck.append(this.sep3_template.clone()); }
-        if (row % 4 == 3) { this.deck.append(this.sep4_template.clone()); }
-        if (row % 5 == 4) { this.deck.append(this.sep5_template.clone()); }
-      }
-    },
-  };
-
-  return self;
+    return self;
 })(writing || {}, jQuery);
